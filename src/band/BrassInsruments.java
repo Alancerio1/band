f6933973aefe942dbd194679a7ad0501fa3786b1
alancerio18 <alancerio18@alex>
2015-09-14 15:40:57 -0500

    made an abstract class and a public class

---
+++ src/band/BrassInsruments.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package band;
+
+/**
+ *
+ * @author alancerio18
+ */
+public abstract class BrassInsruments extends Instrument {
+   private String BrassNames ;
+}
