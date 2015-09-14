da10f343b97a2470b93a6478298b0bb254753bb0
alancerio18 <alancerio18@alex>
2015-09-14 15:52:01 -0500

    Made a Trombone class for the band

--- src/band/Band.java
+++ src/band/Band.java
@@ -15,7 +15,8 @@ public class Band {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Trumpet instrument1 = new Trumpet();
+        
     }
     
 }
