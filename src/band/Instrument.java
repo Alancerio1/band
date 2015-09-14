0be94a1b72cd31bb476d7aa9eb12d8c11f7a8338
alancerio18 <alancerio18@alex>
2015-09-14 15:33:46 -0500

    made an abstract class called instruments

---
+++ src/band/Instrument.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package band;
+
+/**
+ *
+ * @author alancerio18
+ */
+public abstract class Instrument {
+    private String name;
+    
+    
+}
